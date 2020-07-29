f5d986924062489a66917d83ad83dc6435a9f22e
toihocweb <buidinhnhat2031@gmail.com>
2020-07-29 16:15:26 +0700

    #add cors

--- server.js
+++ server.js
@@ -12,23 +12,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "PROD") {
-  // const whitelist = ["https://toihocweb.net"];
-  // const corsOptions = {
-  //   origin: function (origin, callback) {
+  var whitelist = ["https://toihocweb.net", "http://toihocweb.net"];
+  var corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
 
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error("Not allowed by CORS"));
-  //     }
-  //   },
-  // };
-  // app.use(cors(corsOptions));
-  app.use(
-    cors({
-      origin: "https://toihocweb.net",
-    })
-  );
+  app.use(cors(corsOptions));
+  // app.use(
+  //   cors({
+  //     origin: "https://toihocweb.net",
+  //   })
+  // );
 } else {
   app.use(
     cors({
