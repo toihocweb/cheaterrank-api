487c9afe9a1c30532abbfbc874081c69cac11136
toihocweb <buidinhnhat2031@gmail.com>
2020-07-29 14:52:37 +0700

    #add mongo

--- server.js
+++ server.js
@@ -12,17 +12,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "PROD") {
-  const whitelist = ["https://toihocweb.net"];
-  const corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  };
-  app.use(cors(corsOptions));
+  // const whitelist = ["https://toihocweb.net"];
+  // const corsOptions = {
+  //   origin: function (origin, callback) {
+
+  //     if (whitelist.indexOf(origin) !== -1) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error("Not allowed by CORS"));
+  //     }
+  //   },
+  // };
+  // app.use(cors(corsOptions));
+  app.use(
+    cors({
+      origin: "https://toihocweb.net",
+    })
+  );
 } else {
   app.use(
     cors({
