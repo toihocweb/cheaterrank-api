c46e1148f303f7e3fb2acd870c218f1983b6e785
toihocweb <buidinhnhat2031@gmail.com>
2020-08-29 04:06:21 +0700

    #add new url

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://nhatbui:nguoihung@202.182.100.160:27017/cheaterrank",
+  mongoURI: "mongodb://admin:nguoihung@202.182.100.160:27017/cheaterrank",
   secretOrKey: "gtijo3ttret34665rgf",
 };
